c08f9d32bb149f08db4e43ebfea002f7f4c1d6ce
Kevin Decker <kpdecker@gmail.com>
2018-09-30 08:49:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# react-query-selector
+
+React Component selector engine. Provides a `querySelectorAll` type interface for React components rendered into a particular tree.
+
+## Getting started
+
+`npm run dev` builds the library, then keeps rebuilding it whenever the source files change using [rollup-watch](https://github.com/rollup/rollup-watch).
+
+`npm test` builds the library, then tests it.
+
+## License
+
+[MIT](LICENSE).
---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // the `targets` option which can specify `dest` and `format`)
+  {
+    entry: 'src/react-query-selector.js',
+    external: ['ms'],
+    targets: [
+      { dest: pkg.main, format: 'cjs' },
+      { dest: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+];
